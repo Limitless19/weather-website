1cdd6710f74188f7efa4274e1f0d920720a2f609
Tolulope Oke <33998304+Limitless19@users.noreply.github.com>
2019-11-26 17:59:59 +0100

    setting up heroku application'

--- public/js/app.js
+++ public/js/app.js
@@ -13,7 +13,7 @@ weatherForm.addEventListener('submit',(event)=>{
     messageTwo.textContent = ''
 //fetch data from a url.
 //note that fetch is not available in node.it is only available in client side js-which is why we are using it here.
-fetch('http://localhost:3000/weather?address='+ location).then((response)=>{
+fetch('/weather?address='+ location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error)
